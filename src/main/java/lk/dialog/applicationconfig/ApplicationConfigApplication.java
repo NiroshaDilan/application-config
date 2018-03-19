6eefb96589a3c8c211081e5996d95def99fee5e6
DESKTOP-IJ4FOE7\DILAN <dilan.vista@gmail.com>
2018-03-19 15:16:31 +0530

    first commit

---
+++ src/main/java/lk/dialog/applicationconfig/ApplicationConfigApplication.java
@@ -0,0 +1,12 @@
+package lk.dialog.applicationconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationConfigApplication.class, args);
+	}
+}
