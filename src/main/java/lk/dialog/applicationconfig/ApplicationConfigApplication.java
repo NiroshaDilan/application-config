0374734f5e88f7f37652912c56eaf649b2eb32ae
DESKTOP-IJ4FOE7\DILAN <dilan.vista@gmail.com>
2018-03-19 16:05:47 +0530

    properties changes

--- src/main/java/lk/dialog/applicationconfig/ApplicationConfigApplication.java
+++ src/main/java/lk/dialog/applicationconfig/ApplicationConfigApplication.java
@@ -2,8 +2,12 @@ package lk.dialog.applicationconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
 public class ApplicationConfigApplication {
 
 	public static void main(String[] args) {
